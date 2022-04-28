7bee14516fe7c66a0148d63f7f67522239ceeb68
Tyson Tang <a1824273@student.adelaide.edu.au>
2022-04-28 11:02:12 +0930

    commit3

--- Unit.cpp
+++ Unit.cpp
@@ -11,9 +11,24 @@ Unit::Unit(){
 Unit::Unit(int bedroom_number, double square_metres, int dollar_values){
     // a constructor that takes: the number of bedrooms,
     //number of square meters, and value in dollars as parameters
-    bedrooms = bedroom_number;
-    area = square_metres;
-    price = dollar_values;
+    if (bedroom_number >= 0){
+        bedrooms = bedroom_number;
+    }
+    else{
+        bedrooms = 0;
+    }
+    if (square_metres >= 0){
+        area = square_metres;
+    }
+    else{
+        area = 0;
+    }
+    if (dollar_values >= 0){
+        price = dollar_values;
+    }
+    else{
+        price = 0;
+    }
 }
 int Unit::get_bedrooms(){
     // returns the number of bedrooms for the unit
